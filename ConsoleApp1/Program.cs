1debca870174694bc23181943beb4cd1a4b943c9
Dariusz Mierzejewski <md033268@ad.danwood.pl>
2020-10-06 15:35:01 +0200

    at_work

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using Dapper;
 
@@ -10,14 +11,15 @@ namespace sqltest
         {
             try
             {
-                string connectionString = "Data Source=localhost;Initial Catalog=AdventureWorksLT2019;Integrated Security=True";
+                // string connectionString = "Data Source=localhost;Initial Catalog=AdventureWorksLT2019;Integrated Security=True";
+                string connectionString = "Data Source=localhost;Initial Catalog=AdventureWorksDW2017;Integrated Security=True"; 
                 using SqlConnection connection = new SqlConnection(connectionString);
 
-                string querySql = "SELECT TOP (10) [CustomerID],[FirstName],[LastName],[CompanyName],[EmailAddress],[Phone] FROM[AdventureWorksLT2019].[SalesLT].[Customer]";
-                var users = connection.Query(querySql);
+                string querySql = "SELECT TOP (10) [EmployeeKey],[FirstName],[LastName],[Title],[BirthDate],[EmailAddress],[Phone],[Status] FROM [dbo].[DimEmployee]";
+                IEnumerable<User> users = connection.Query<User>(querySql);
                 foreach (var user in users)
                 {
-                    Console.WriteLine($"{user.CustomerID} {user.FirstName} {user.LastName} {user.CompanyName} {user.EmailAddress}");
+                    Console.WriteLine($"{user.EmployeeKey} {user.FirstName} {user.LastName} {user.Title} {user.EmailAddress} {user.BirthDate} {user.Phone} {user.Status}");
                 }
             }
             catch (SqlException e)
@@ -27,5 +29,19 @@ namespace sqltest
             Console.WriteLine("\nDone. Press enter.");
             Console.ReadLine();
         }
+    }
+
+    internal class User
+    {
+        public int EmployeeKey { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+
+        public string FullName { get => $"{LastName} {FirstName}"; }
     }
 }
